ceded346bef1ea748e234f01eff9fcf6f9d37cdc
adura@omnixcorp.com <aduran@omnixcorp.com>
2023-03-17 00:13:55 -0500

    env

--- src/classes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as App } from "./App"
-export { default as Intersection } from "./Intersection"
-export { default as Resizer } from "./Resizer"
-export { default as Toolbar } from "./Toolbar"
